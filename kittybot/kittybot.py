cbd66292da01037313d4e5529c4afc83ed577fdf
maximusishere <maxim.yarm@gmail.com>
2024-08-02 21:09:48 +0300

    improved security

--- kittybot/kittybot.py
+++ kittybot/kittybot.py
@@ -1,17 +1,30 @@
-from telebot import TeleBot
+import os
+from dotenv import load_dotenv
+from telebot import TeleBot, types
 import requests
 
+load_dotenv()
 
-bot = TeleBot(token='1659639915:AAHLGCTJMVCBPqtTid2Mi-rpuHBu6IISew0')
+secret_token = os.getenv('TOKEN')
 
-URL = 'https://api.thecatapi.com/v1/images/search'
+URL = os.getenv('URL')
+
+bot = TeleBot(token=secret_token)
 
 # Код запроса к thecatapi.com и обработку ответа обернём в функцию:
+
+
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        print(error)      
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
-
 
 # Добавляем хендлер для команды /newcat:
 @bot.message_handler(commands=['newcat'])
@@ -24,10 +37,14 @@ def new_cat(message):
 def wake_up(message):
     chat = message.chat
     name = message.chat.first_name
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_newcat = types.KeyboardButton('/newcat')
+    keyboard.add(button_newcat, row_width=3)
 
     bot.send_message(
         chat_id=chat.id,
         text=f'Привет, {name}. Посмотри, какого котика я тебе нашёл',
+        reply_markup=keyboard,
     )
 
     bot.send_photo(chat.id, get_new_image())
@@ -39,45 +56,5 @@ def say_hi(message):
     chat_id = chat.id
     bot.send_message(chat_id=chat_id, text='Привет, я KittyBot!')
 
+
 bot.polling()
-# chat_id = 1669837246
-# message = 'Вам телеграмма!'
-# # Вызываем метод send_message, с помощью этого метода отправляются сообщения:
-# bot.send_message(chat_id, message)
-
-# @bot.message_handler(commands=['start'])
-# def wake_up(message):
-#     chat = message.chat
-#     name = chat.first_name
-#     chat_id = chat.id
-#     bot.send_message(chat_id=chat_id, text=f'Спасибо, что включили меня, {name}!')
-
-# @bot.message_handler(content_types=['text'])
-# def say_hi(message):
-#     chat = message.chat
-#     chat_id = chat.id
-#     bot.send_message(chat_id=chat_id, text='Привет, я Kittybot!')
-URL = 'https://api.thecatapi.com/v1/images/search'
-response = requests.get(URL).json()
-
-print(response)
-
-# response — это список. Удостоверимся:
-print(type(response))
-
-# Какой длины список response?
-print(len(response))
-
-# Посмотрим, какого типа первый элемент:
-print(type(response[0]))
-
-# chat_id = 1669837246
-# text = 'Вам телеграмма!'
-# # Отправка сообщения
-# bot.send_message(chat_id, text)
-# # Отправка изображения
-# bot.send_photo(chat_id, URL)
-
-
-
-# bot.polling()
